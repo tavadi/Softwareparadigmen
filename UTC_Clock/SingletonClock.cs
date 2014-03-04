a4a469ce3278b5bd7a55cff48a9e8b1d51dfd3ea
tavadi <t_beni@hotmail.com>
2014-03-04 20:02:53 +0100

    first commit
    basic singleton implementation

---
+++ UTC_Clock/SingletonClock.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UTC_Clock
+{
+    public class SingletonClock
+    {
+      private static SingletonClock instance;
+
+      private SingletonClock() { }
+
+      public static SingletonClock Instance
+       {
+          get 
+          {
+             if (instance == null)
+             {
+                 instance = new SingletonClock();
+             }
+             return instance;
+          }
+       }
+    }
+}
